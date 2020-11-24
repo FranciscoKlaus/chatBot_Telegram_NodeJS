69d9000574c0f1dfd13105cbc78fc7a591927747
Mac <mac@MacBook-Air-de-Mac.local>
2020-11-24 20:42:41 -0300

    arquivo de eventos com os eventos photo,message,contact,location,sticker,voice,text

--- exercicios/evento.js
+++ exercicios/evento.js
@@ -14,9 +14,36 @@ bot.on('text', ctx =>{
 
 bot.on('location', ctx =>{
     const location = ctx.update.message.location
+    console.log(location)
     ctx.reply(`Entendido. Você está em:
     Lat: ${location.latitude},
     Long: ${location.longitude} !`)
+})
+
+bot.on('contact', ctx =>{
+    const contact = ctx.update.message.contact
+    console.log(contact)
+    ctx.reply(`Vou lembra do(a) ${contact.first_name} (${contact.phone_number})`)   
+})
+
+bot.on('voice', ctx => {
+    const voice = ctx.update.message.voice
+    console.log(voice)
+    ctx.reply(`O áudio dura: ${voice.duration} segundos`)
+})
+ 
+bot.on('photo', ctx => {
+    const photo = ctx.update.message.photo
+    console.log(photo)
+    photo.forEach((picture, i) => {
+        ctx.reply(`Photo ${i} tem resolução ${picture.width}x${picture.height}`)
+    })
+})
+
+bot.on('sticker', ctx => {
+    const sticker = ctx.update.message.sticker
+    console.log(sticker)
+    ctx.reply(`Estou vendo que você enviou o ${sticker.emoji} do conjunto ${sticker.set_name}`)
 })
 
 bot.startPolling() 
