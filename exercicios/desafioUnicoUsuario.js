a4f46a794924884c7232f7b0cb391665ebbceec0
Mac <mac@MacBook-Air-de-Mac.local>
2020-11-24 19:12:40 -0300

    correções nos arquivos

---
+++ exercicios/desafioUnicoUsuario.js
@@ -0,0 +1,21 @@
+//verificar o usuário ID
+//se o id for o consultado responder = Ao seu dispor mestre
+//caso não seja o ID - responder = Sinto muito, mas eu só falo com meu mestre.
+
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+let idFrom = [643571007,689186643]
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+    console.log(from.id)
+    if(idFrom.indexOf(from.id) !== -1){
+        ctx.reply(`Seja bem vindo, ${from.first_name}!`)
+    }else{
+        ctx.reply(`Sinto muito, mas eu só falo com o meu Mestre!`)
+    }
+    
+})
+
+bot.startPolling()
